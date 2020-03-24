253866b0e159d43f4fff33ec1084e3496c934dcd
zhangchao <dzzchao@163.com>
2020-03-25 00:59:56 +0800

    Proxy

--- app/src/main/java/com/dzzchao/fwanandroid/App.kt
+++ app/src/main/java/com/dzzchao/fwanandroid/App.kt
@@ -3,12 +3,15 @@ package com.dzzchao.fwanandroid
 import android.app.Application
 import android.content.Context
 import timber.log.Timber
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
 
 const val BASEURL = "https://www.wanandroid.com"
 
 class App : Application() {
 
-    companion object{
+    companion object {
         lateinit var context: Context
     }
 
@@ -19,6 +22,19 @@ class App : Application() {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+
+            val clazz = ActivityLifecycleCallbacks::class.java
+            val callback = Proxy.newProxyInstance(
+                clazz.classLoader,
+                arrayOf(clazz)
+            ) { _, method, args ->
+                Timber.d(
+                    "LifeCycle: %s  %s", args!![0].javaClass.simpleName,
+                    method?.name
+                )
+                Unit
+            } as ActivityLifecycleCallbacks
+            registerActivityLifecycleCallbacks(callback)
         }
     }
 }
